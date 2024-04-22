cb4171e37557aa4dd102b6e37f9e965abc0eeee0
yayuli <pengyufentf@outlook.com>
2024-04-22 17:15:02 -0600

    update i generator

---
+++ Assets/Script/IGenerator.cs
@@ -0,0 +1,5 @@
+public interface IGenerator
+{
+    void Initialize(MazeGenerator mazeGenerator);
+    void Generate();
+}
--- Assets/Script/Maze/ObstaclesGenerate.cs
+++ Assets/Script/Maze/ObstaclesGenerate.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObstaclesGenerate : MonoBehaviour
+public class ObstaclesGenerate : MonoBehaviour, IGenerator
 {
     [SerializeField] private GameObject obstaclePrefab;
     [SerializeField] private int numberOfObstacles = 10;
@@ -12,18 +12,26 @@ public class ObstaclesGenerate : MonoBehaviour
     public void Initialize(MazeGenerator mazeGenerator)
     {
         this.mazeGenerator = mazeGenerator;
-        PlaceObstaclesRandomly();
+       
     }
 
+    public void Generate()
+    {
+        PlaceObstaclesRandomly();
+    }
     private void PlaceObstaclesRandomly()
     {
         List<Vector2> availablePositions = new List<Vector2>(mazeGenerator.availablePositions);
         for (int i = 0; i < numberOfObstacles && availablePositions.Count > 0; i++)
         {
-            int randomIndex = Random.Range(0, availablePositions.Count);
-            Vector2 obstaclePosition = availablePositions[randomIndex];
-            Instantiate(obstaclePrefab, obstaclePosition, Quaternion.identity);
-            availablePositions.RemoveAt(randomIndex);
+            if (availablePositions.Count > 0)
+            {
+                int randomIndex = Random.Range(0, availablePositions.Count);
+                Vector2 obstaclePosition = availablePositions[randomIndex];
+                Instantiate(obstaclePrefab, obstaclePosition, Quaternion.identity);
+                availablePositions.RemoveAt(randomIndex);
+            }
+                
         }
     }
 }
