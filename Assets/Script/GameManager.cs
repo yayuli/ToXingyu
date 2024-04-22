eeba7fe70affcff70d058371086e8412f8d750c4
yayuli <pengyufentf@outlook.com>
2024-04-22 17:15:02 -0600

    update i generator

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,23 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private IGenerator[] generators;
     [SerializeField] private MazeGenerator mazeGenerator;
-    [SerializeField] private ObstaclesGenerate obstaclesGenerator;
-    [SerializeField] private WeaponsGenerate weaponsGenerator;
-    [SerializeField] private EnemysGenerate enemysGenerator;
 
     void Awake()
     {
-        // Ensure MazeGenerator is initialized first
-        mazeGenerator.GenerateMaze(mazeGenerator.mazeRows, mazeGenerator.mazeColumns);
+        // Automatically find all IGenerator implementations in the scene
+        generators = FindObjectsOfType<MonoBehaviour>().OfType<IGenerator>().ToArray();
+        InitializeGenerators();
+    }
 
-        // Inject MazeGenerator into other components
-        obstaclesGenerator.Initialize(mazeGenerator);
-        weaponsGenerator.Initialize(mazeGenerator);
-        enemysGenerator.Initialize(mazeGenerator);
+    private void InitializeGenerators()
+    {
+        if (mazeGenerator != null)
+        {
+            // Ensure MazeGenerator is initialized first
+            mazeGenerator.GenerateMaze(mazeGenerator.mazeRows, mazeGenerator.mazeColumns);
+
+            // Initialize and generate for all generators found
+            foreach (var generator in generators)
+            {
+                generator.Initialize(mazeGenerator);
+                generator.Generate();
+            }
+        }
+        else
+        {
+            Debug.LogError("MazeGenerator instance not found!");
+        }
     }
 }
-
